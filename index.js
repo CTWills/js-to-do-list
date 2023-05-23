95cc3a251d37ebf7544bfe17035590b037533818
CTWills <cameron.wills.1998@gmail.com>
2023-05-22 19:58:43 -0600

    created event listeners for the button and input text

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 // 2. The input needs to be added to a list the user sees
 // 3. User needs to remove to-do items
 
+let submitButton = document.querySelector('#submit-button');
+let inputTextField = document.querySelector('#input-text')
+let todoList = document.querySelector('#list');
+let inputTextValue = '';
+
+
+const buttonFunction = () => {
+    console.log(inputTextValue);
+}
+
+submitButton.addEventListener('click', buttonFunction);
+
+inputTextField.addEventListener('input', (eventObject) => {
+    inputTextValue = eventObject.target.value;
+    return inputTextValue;
+})
